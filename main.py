033498401e095578d218dd87123007ab7a6a429f
andyoudontseemtounderstand <8033@khaitanpublicschool.com>
2026-04-28 08:08:40 +0530

    Fahhhh

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st 
-st.title("Welcome to Streamlit")
-st.write("Uhh hi?")
+st.title("Meow")
+st.write("Now this looks like a job for me, so everybody, just follow me, 'cause we need a little bit o'controversy, 'cause it feels so empty without me!")
