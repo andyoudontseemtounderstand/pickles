7c5c81351ece9c42ecb00729d6d88f783b42576f
andyoudontseemtounderstand <8033@khaitanpublicschool.com>
2026-04-28 08:17:11 +0530

    selling info v2

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ st.title("Meow")
 st.write("Now this looks like a job for me, so everybody, just follow me, 'cause we need a little bit o'controversy, 'cause it feels so empty without me!")
 st.write()
 st.write()
-name = st.text_input("What's your name?: ")
-age = st.number_input("What's your age?: ")
-
-if st.button("Press this so I can sell your info for free money"):
-  st.write("Thanks")
+with st.form("my_form"):
+    name = st.text_input("Name (Required)")
+    email = st.text_input("Email (Required)")
+    
+    # Every form must have a submit button
+    submitted = st.form_submit_button("Submit")
+    
+    if submitted:
+        if not name or not email:
+            st.error("Please fill in all required fields.")
+        else:
+            st.success(f"Form submitted for {name}!")
