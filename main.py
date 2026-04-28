a4f1ba8b81e493fc61cf543d08c40727892b1aa1
andyoudontseemtounderstand <8033@khaitanpublicschool.com>
2026-04-28 08:21:39 +0530

    selling info v4

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ st.write("Now this looks like a job for me, so everybody, just follow me, 'cause
 st.write()
 st.write()
 with st.form("my_form"):
-    name = st.text_input("Name (Required)")
-    email = st.text_input("Email (Required)")
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    age = st.number_input("Enter your age", value=0, min_value=0, step=1)
     
     # Every form must have a submit button
     submitted = st.form_submit_button("Press this so I can sell your info")
     
     if submitted:
         if not name or not email:
-            st.error("Please fill in all required fields.")
+            st.error("Hey! I can't sell your info without a filled form")
         else:
             st.success(f"Form submitted for {name}!")
